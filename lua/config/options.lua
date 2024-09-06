8134166fef7e361f6583ce90d9c5dc24c08e0450
valar <68338647+76616c6172@users.noreply.github.com>
2024-09-05 17:22:01 -0700

    fix(*): remove stupid autocmd and set options directly

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,16 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
 vim.opt.cursorline = false
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.opt.shiftwidth = 2
-    vim.opt.tabstop = 2
-    vim.opt.softtabstop = 0
-    vim.opt.shiftwidth = 2
-    vim.opt.expandtab = false
-    vim.opt.guifont = "MononokiNerdFontMono-Regular"
-  end,
-})
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 0
+vim.opt.expandtab = false
+vim.opt.guifont = "MononokiNerdFontMono-Regular"
